808445dd18e6bae7fbca17505dbe30bf90b349b3
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-26 18:24:59 +0500

    add dependancy injection

---
+++ Garaj.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Castle.Core.Configuration;
+using Garaj.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+namespace Garaj.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastruct(this IServiceCollection services)
+        {
+            services.AddDbContext<GarajDbContext>(ops =>
+                ops
+                    .UseLazyLoadingProxies()
+                        .UseNpgsql(connectionString: "Host=localhost;Port=5432;Database=Car.BLZ;User Id=postgres;Password=root"));
+
+
+            return services;
+        }
+    }
+}
