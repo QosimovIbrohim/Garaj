8fc638c21e5e366987559fbd85efad13c1388e43
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-26 18:25:20 +0500

    update domain

---
+++ Garaj.Domain/Entities/Auth/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaj.Domain.Entities.Auth
+{
+    public class User : IdentityUser<Guid>
+    {
+        public string FullName { get; set; }
+        public virtual List<Car> Cars { get; set; }
+    }
+}
+
---
+++ Garaj.Domain/Entities/Car.cs
@@ -0,0 +1,26 @@
+﻿using Garaj.Domain.Entities.Auth;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaj.Domain.Entities
+{
+    public class Car
+    {
+        public Guid Id = Guid.NewGuid();
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Guid OwnerId { get; set; }
+        public bool isSold { get; set; } = false;
+
+        [ForeignKey(nameof(OwnerId))]
+        public virtual User Owner { get; set; }
+
+    }
+}
