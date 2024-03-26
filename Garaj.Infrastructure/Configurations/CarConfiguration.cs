fa9f42c3389e549bf224b62e2e3d61609782e171
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-26 18:24:38 +0500

    add configurations

---
+++ Garaj.Infrastructure/Configurations/CarConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Garaj.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaj.Infrastructure.Configurations
+{
+    public class CarConfiguration : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Description)
+                .HasMaxLength(120)
+                .IsRequired(false);
+
+            builder.Property(x => x.Model)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            builder.Property(x => x.Brand)
+                .HasMaxLength(30)
+                .IsRequired();
+            builder.HasData(new Car()
+            {
+                Model = "Damas",
+                Brand="Chevrolet",
+                Description = "Zo'r buxanka",
+                Price=60000
+            });
+        }
+    }
+}
