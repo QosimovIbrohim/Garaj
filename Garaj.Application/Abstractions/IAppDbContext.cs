63d2bc0391ed60010392ef44b4afcb8fab689125
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-26 18:27:03 +0500

    add appdbcontext

---
+++ Garaj.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Garaj.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaj.Application.Abstractions
+{
+    public interface IAppDbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        ValueTask<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
