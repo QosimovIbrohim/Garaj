08c954ac2fdc5893c760302af6c6789cb4a60e56
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-26 18:26:13 +0500

    add create command

---
+++ Garaj.Application/useCases/CarCase/Command/CreateCarCommand.cs
@@ -0,0 +1,19 @@
+﻿using Garaj.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaj.Application.useCases.CarCase.Command
+{
+    public class CreateCarCommand : IRequest<Car>
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Guid OwnerId { get; set; }
+    }
+}
---
+++ Garaj.Application/useCases/CarCase/Command/CreateCarCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Garaj.Application.Abstractions;
+using Garaj.Domain.Entities;
+using Garaj.Domain.Entities.Auth;
+using Mapster;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garaj.Application.useCases.CarCase.Command
+{
+    public class CreateCarCommandHandler : IRequestHandler<CreateCarCommand, Car>
+    {
+        private readonly IAppDbContext _appDbContext;
+
+        public CreateCarCommandHandler(IAppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Car> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            var car = request.Adapt<Car>();
+
+            _appDbContext.Cars.Add(car);
+            await _appDbContext.SaveChangesAsync(cancellationToken);
+
+            return car;
+        }
+    }
+}
